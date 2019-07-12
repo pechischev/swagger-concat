7d9d453b1e637874f8eec103ce86a4863f998458
pechischev_vladimir <vladimir.pechischev@omega-r.com>
2019-07-12 17:15:59 +0300

    [TASS-20] append list docs

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const path = require('path');
 const swaggerUi = require('swagger-ui-express');
 const docs = require('./api');
-
+const config = require('./config.json');
 
 const app = express();
+
+const baseUrl = config.url;
+const options = {
+    definitions: `${baseUrl}/doc/definitions`,
+    common: `${baseUrl}/doc/common`,
+    customer: `${baseUrl}/doc/customer`,
+};
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, "views"));
+app.get('/doc', (req, res) => {
+    res.render('doc', options);
+});
 
 async function loadDocs() {
     for (const key of Object.keys(docs)) {
